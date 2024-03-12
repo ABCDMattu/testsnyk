8aa1ddf4fd8bafe41cf4b69d1d3ca93f3f1c9e03
ABCDMattu <118290688+ABCDMattu@users.noreply.github.com>
2024-03-12 23:38:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ current_time = datetime.now(ist).isoformat()
 table_name = 'bitcoin_price_storer'
 # Set the REST API endpoint
 api_url = 'https://api.coinbase.com/v2/prices/btc-usd/spot'
-api_key="123456789"
-password="Hello"
+api_key1="123456789"
+api_key2="123456789"
 # Create a DynamoDB client
 dynamodb = boto3.client('dynamodb',region_name=region_name)
 
